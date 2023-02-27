fb647e9b0726e7be8464cb38a1f1ff0674b5f43e
Alex Baskakau <lenovos6000v4@gmail.com>
2023-02-27 14:28:36 +0000

    Twin versions

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public int calculate(double a, double b ) {
+        int c = (int) (a / (b * b));
+        return c;
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+       // double weight = 98;// вес в килограммах
+       // double height = 1.87;// рост в метрах
+
+       // int index = service.calculate(weight, height);
+       // System.out.println( "Индекс массы тела= "+ (index));
+        System.out.println(service.calculate(98, 1.87));
+    }
+
+
+}
